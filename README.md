5ff558e80222f17d822e622d0d13107e94e3ab5c
Reece Shuttleworth <reece.shuttle@gmail.com>
2023-10-13 15:48:26 -0400

    pushing changes

--- README.md
+++ README.md
@@ -4,17 +4,18 @@
 
 Usage:
 
-- create venv:
+- create and activate venv:
 
 ```
-python -m venv 958 
+python -m venv 958
 source 958/bin/activate
 ```
 
 - install dependencies:
 
 ```
-pip install torch  
+pip install torch
 pip install transformers
 pip install einops
+pip install scipy
 ```
---
+++ utils.py
@@ -0,0 +1,35 @@
+import torch
+import numpy.linalg as linalg
+from scipy.stats import entropy
+import numpy as np
+
+def calc_entropy(M: torch.Tensor):
+    """
+    Calculates the row-wise entropy of a matrix.
+    """
+    raise NotImplementedError('Implement me!')
+
+def calc_presoftmax_entropy(A: torch.Tensor):
+    """
+    Calculates the row-wise entropy before softmax.
+    A is the matrix of activation scores before softmax'd.
+    A = X(W_q)(W_k^T)X^T
+    1. We apply probability dist: P_i = |a_{1i}|/\sum^n_{j=1}|a_{1j}|
+    2. using these probabilities, we calculate entropy.
+    note that doing the scaling here doesnt matter with the probability dist
+    we use since it factors out.
+    """
+    A_ = np.abs(A.detach())
+    P = A_/np.sum(A_, axis=1, keepdims=True)
+    return entropy(P, axis=1)
+
+def calc_stable_rank(M: torch.Tensor) -> float:
+    """
+    Takes a matrix M and calculates the stable rank of it:
+    stable_rank = (sv_1+...+sv_n)/sv_1,
+    where sv_1 is the biggest singular value of matrix M.
+    ref: https://www.cs.ubc.ca/~nickhar/W12/Lecture15Notes.pdf
+    """
+    _, S, _ = linalg.svd(M.detach())
+    stable_rank = sum([val**2 for val in S])/S[0]**2
+    return stable_rank
